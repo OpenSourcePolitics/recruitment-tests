528f7cfd0798279244643d3971b38e78995672d0
kingsleyDeve <kingsley.williams.pro@gmail.com>
2019-08-26 04:32:18 +0200

    exercices ruby level-o terminé et level-a en cours

--- ruby/level-a/main.rb
+++ ruby/level-a/main.rb
@@ -1,4 +1,12 @@
 class Test
   def fibo(n)
+
+    if n > 0
+      n = n + n.last
+
+    else
+      n = n
+    end
+
   end
 end
--- ruby/level-o/main.rb
+++ ruby/level-o/main.rb
@@ -1,4 +1,18 @@
 class Test
   def smallest_or_highest(arr, sort)
+
+
+      if sort == "min"
+      arr = arr.min
+
+    elsif sort == "max"
+      arr = arr.max
+
+    else
+      arr = arr
+
+    end
+
+
   end
 end
