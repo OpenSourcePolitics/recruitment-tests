f53dd7414d41a39d2784b894f9fed375a55a8780
dynnammo <baptiste.thivend@insa-lyon.fr>
2020-07-02 18:06:42 +0200

    Ruby - Resolve Level-f

--- ruby/level-f/main.rb
+++ ruby/level-f/main.rb
@@ -1,4 +1,14 @@
 class Test
   def rot13(string)
+    alphabeat = "abcdefghijklmnopqrstuvwxyz" 
+    
+    start_alphabeat = alphabeat + alphabeat.swapcase()
+    end_alphabeat = alphabeat.chars.rotate(13).join() + alphabeat.swapcase().chars.rotate(13).join()
+  
+    new_string = ""
+    for char in string.chars
+      new_string += start_alphabeat.index(char) ? end_alphabeat[start_alphabeat.index(char)] : char
+    end
+    return new_string
   end
 end
