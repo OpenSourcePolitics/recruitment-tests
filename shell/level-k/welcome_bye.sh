fd9b79ed9baf26fd1c0e0005a0d9ce2a64f54aab
Armand Fardeau <fardeauarmand@gmail.com>
2020-09-11 14:09:50 +0200

    Prefer a case switch

--- shell/level-k/welcome_bye.sh
+++ shell/level-k/welcome_bye.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-if [ "x$1" == "xhello" ]; then
-	echo Welcome $(whoami)
-elif [ "x$1" == "xbye" ]; then
-	echo Byebye $(whoami)
-elif [ "x$1" == "x" ]; then 
-	echo "No argument provided, script requires argument"
-	exit 1
-fi
-
+case $1 in
+"hello")
+echo Welcome $(whoami)
+;;
+bye)
+echo Byebye $(whoami)
+;;
+*)
+echo "No argument provided, script requires argument"
+exit 1
+;;
+esac
 
