b0e2d663c0bee4004a82510f23993ec1b9af95ed
eliegab <gaboriaueli@gmail.com>
2021-05-14 19:34:03 +0200

    level f

--- ruby/level-f/main.rb
+++ ruby/level-f/main.rb
@@ -1,4 +1,15 @@
 class Test
   def rot13(string)
+    ciphered=""
+    string.split('').each{ |c|
+      if ((c.ord < 78 && c.ord >64) || (c.ord <110 && c.ord>96))
+        ciphered+=(c.ord+13).chr
+      elsif ((c.ord > 77 && c.ord < 91) || (c.ord > 109 && c.ord < 123))
+        ciphered+=(c.ord-13).chr
+      else
+	ciphered+=c
+      end
+    }
+  return ciphered
   end
 end
