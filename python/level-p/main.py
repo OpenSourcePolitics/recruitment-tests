353a277745cebc6a1ecfa4fd49fa490f747ab68e
azouiaymen <azouiaimen97@gmail.com>
2021-09-06 20:43:29 +0200

    aymen azoui test

--- python/level-p/main.py
+++ python/level-p/main.py
@@ -1,2 +1,5 @@
 def fizzbuzz(n):
-    pass
+    result = []
+    for x in range(1,n+1):
+        result.append(("fizzbuzz" if x%15==0 else "fizz" if x%3==0 else "buzz" if x%5==0 else str(x)))
+    return result
