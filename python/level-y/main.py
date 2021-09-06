b07407ee3219d2b504fb317435b892b34b56b9b9
azouiaymen <azouiaimen97@gmail.com>
2021-09-06 21:22:27 +0200

    aymen azoui test

--- python/level-y/main.py
+++ python/level-y/main.py
@@ -1,2 +1,4 @@
+import statistics
+
 def compute_temperature_data(temperature_data):
-    pass
+    return [round(statistics.median(temperature_data),1),round(statistics.mean(temperature_data),1),round(min(temperature_data),1),round(max(temperature_data),1)]
