0cb5e96b75e2d334fef7bbd9f8429af00725fe74
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-10-31 13:29:08 +0100

    Conditions ok mais
    pb de présentation des réponses

--- python/level-p/main.py
+++ python/level-p/main.py
@@ -2,18 +2,19 @@
 # def fizzbuzz(n):
 #     pass
 
-# test failed : fizzbuzz ne passe pas
+# test failed : fizzbuzz ok, mais
+# la réponse n'apparaît pas en tableau
 
 def fizzbuzz (n) :
     if n <= 0 :
         return []
     else :
-        for x in range(n) :
-            if (x % 3 == 0):
+        for x in range(n+1) :
+            if (x % 3 == 0 and x % 5 == 0):
+                print ('fizzbuzz')
+            elif (x % 3 == 0):
                 print('fizz')
             elif (x % 5 == 0):
                 print ('buzz')
-            elif (x % 3 == 0 and x % 5 == 0):
-                print ('fizzbuzz')
             else:
                 print (x)
