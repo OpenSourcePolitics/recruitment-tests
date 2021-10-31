3ffe1e35c4605ace0e97b92f8da293982f444846
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-10-31 12:30:54 +0100

    test ok pour fizz et buzz
    pas pour fizzbuzz -> en cours

--- python/level-p/main.py
+++ python/level-p/main.py
@@ -1,2 +1,16 @@
-def fizzbuzz(n):
-    pass
+# code initial :
+# def fizzbuzz(n):
+#     pass
+
+# test failed -> 2 problèmes :
+# n passe les 3 conditions et est donc printé 3x
+# fizzbuzz ne passe
+
+def fizzbuzz (n) :
+    if n <= 0 :
+        return []
+    else :
+        for x in range(n) :
+            print ('fizz' if x % 3 == 0 else x)
+            print ('buzz' if x % 5 == 0 else x)
+            print ('fizzbuzz' if x % 3 == 0 and x % 5 == 0 else x)
