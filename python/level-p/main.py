99ded8e0c21cac3c016811cca3f6cad4437bd365
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-10-31 12:53:27 +0100

    Pb des 3 tours de boucles réglées
    (voir commentaires dans code précédent)
    Par contre 'fizzbuzz' ne passe toujours pas

--- python/level-p/main.py
+++ python/level-p/main.py
@@ -2,15 +2,18 @@
 # def fizzbuzz(n):
 #     pass
 
-# test failed -> 2 problèmes :
-# n passe les 3 conditions et est donc printé 3x
-# fizzbuzz ne passe
+# test failed : fizzbuzz ne passe pas
 
 def fizzbuzz (n) :
     if n <= 0 :
         return []
     else :
         for x in range(n) :
-            print ('fizz' if x % 3 == 0 else x)
-            print ('buzz' if x % 5 == 0 else x)
-            print ('fizzbuzz' if x % 3 == 0 and x % 5 == 0 else x)
+            if (x % 3 == 0):
+                print('fizz')
+            elif (x % 5 == 0):
+                print ('buzz')
+            elif (x % 3 == 0 and x % 5 == 0):
+                print ('fizzbuzz')
+            else:
+                print (x)
