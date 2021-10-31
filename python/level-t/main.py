eee3f35e31cdd5904a20bece7b5acfc3373c1b25
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-10-31 20:13:08 +0100

    tests passed for word only
    need to continue for sentence !

--- python/level-t/main.py
+++ python/level-t/main.py
@@ -1,2 +1,10 @@
-def is_palindrome(string):
-    pass
+# def is_palindrome(string):
+#     pass
+
+
+def is_palindrome (string) :
+    
+    comparison_str = string [::-1]
+    
+    if string == comparison_str:
+        return True
