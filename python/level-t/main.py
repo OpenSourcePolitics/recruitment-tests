1fb5d64820bced5a131644721eeef3b1d76f7702
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-11-01 17:17:53 +0100

    Special characters : removed
    need to check about whitespaces

--- python/level-t/main.py
+++ python/level-t/main.py
@@ -1,13 +1,14 @@
+import re
+
 # def is_palindrome(string):
 #     pass
-
-
 
 def is_palindrome (string) :
     
     lower_case_str = string.lower()
-    comparison_str = lower_case_str [::-1]
-    
+    #letter_only_str = lower_case_str.replace("!" "-", " ")
+    letter_only_str = re.sub("[^a-z]", " ", lower_case_str)
+    comparison_str = letter_only_str [::-1]
 
-    if lower_case_str == comparison_str:
+    if letter_only_str == comparison_str:
         return True
