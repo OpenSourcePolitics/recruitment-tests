ce12f6c6e9ff6b0feade5328b35843145d640d30
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-11-01 17:21:29 +0100

    Test PASSED

--- python/level-t/main.py
+++ python/level-t/main.py
@@ -1,14 +1,12 @@
 import re
 
-# def is_palindrome(string):
-#     pass
-
 def is_palindrome (string) :
     
     lower_case_str = string.lower()
-    #letter_only_str = lower_case_str.replace("!" "-", " ")
     letter_only_str = re.sub("[^a-z]", " ", lower_case_str)
-    comparison_str = letter_only_str [::-1]
+    no_whitespace_str = letter_only_str.replace(" ", "")
 
-    if letter_only_str == comparison_str:
+    comparison_str = no_whitespace_str [::-1]
+
+    if no_whitespace_str == comparison_str:
         return True
