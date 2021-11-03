214d83c25567c259ce82565eed0a289066290b73
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-11-03 13:41:17 +0100

    Début - toujours en cours
    Création d'une class Project pour isoler chaque total de vote, avant de créer une classe qui fera les comparaisons.
    On peut ainsi gérer autant de projets que nécessaire, ajouter des propriétés si nécessaires, connaître les votes correspondants.
    Dans un second temps, on peut isoler plus facilement le/ les projets retenus.
    
    Test visuel avec ligne 14, 34 et 35

--- python/level-h/main.py
+++ python/level-h/main.py
@@ -1,2 +1,59 @@
-def compute_executable_projects(n, participants_votes, project_range):
-    pass
+import numpy as np
+
+# def compute_executable_projects(n, participants_votes, project_range):
+#     pass
+
+# EXAMPLES INPUT AND RESULTS
+# (2, [(10,5),(0,3)],2) == [1,2]
+# (3, [(2,3,4),(7,10,2),(0,0,0)], 1) == [2]
+# (1, [(8,9)], 0) == 'No projects, sorry!'
+
+# DETAILS : [(votes of participant 1), (votes of participants 2), ...]
+
+
+participants_votes = [(10,5),(1,3)]
+
+
+class Project:
+# Project box with rank and votes per participants
+    def __init__ (self, rank):
+        self.rank = rank
+        self.votes = self.counted_votes()
+
+# for each tuple in array particpants_votes
+# pick the vote of rank-1   
+    def counted_votes(self):
+        counting = np.array(participants_votes)
+        vote = 0
+        i = self.rank -1
+        
+        for tuple in counting:
+            vote += tuple[i]
+            print (vote)
+
+test = Project (1)
+test.counted_votes()
+
+# class Most_Supported_Project:
+# #Project with the most of votes
+#     def __init__ (self):
+
+
+
+# def compute_executable_projects (n, participants_votes, project_range):
+# # n = number of particpants
+    
+    
+#     set_votes = (tuple de votes)
+#     #tuples are ordered and unchangeable data
+
+#     project_range = number 
+
+
+
+#     if n != len(participants_votes):
+#         print ("It seems that we don't get all the votes !")
+
+#     if project_range == 0:
+#         print ("No projects, sorry")
+    
