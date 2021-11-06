262f8ba65db25cf4ef1af616b8ffe6c9aed1bece
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-11-06 18:48:36 +0100

    Problem : no iteration over the votes set
    -> counting ok only for project 1

--- python/level-h/main.py
+++ python/level-h/main.py
@@ -3,47 +3,43 @@ import numpy as np
 # def compute_executable_projects(n, participants_votes, project_range):
 #     pass
 
-# # #input :
-# details_set = (2, [(10,5),(0,3)], 2) 
 
-# # # expected output
-# # # == [1,2]
-
-# n = details_set[0]
-# participants_votes = details_set[1]
-# project_range = details_set[2]
+# # #input example for visual tests :
+details_set = (2, [(10,5),(1,3)], 2) 
 
 
-def compute_executable_projects (n, participants_votes, project_range):
+n = details_set[0]
+participants_votes = details_set[1]
+project_range = details_set[2]
+
+print ("number of participants : " + str(n))
+print ("array of votes, per participants : " + str(participants_votes))
+print ("number of allocated budget : " + str(project_range))
+
+
+def counted_votes():
+    counting = np.array(participants_votes)
+    vote = 0
+    i = 0
+    result = []
+    for tuple in counting:
+        vote += tuple[i]
+    result.append(vote)
+    print (result)
+
+
+def compute_executable_projects (project_range):
     if project_range == 0:
-        return ('No projects, sorry!')
+        print ('No projects, sorry!')
     else:
-        return ('Votes : ok')
+        counted_votes ()
 
 
-class Project:
-
-    def __init__ (self, rank):
-        self.rank = rank
-
-    def counted_votes(self):
-        counting = np.array(participants_votes)
-        vote = 0
-        i = self.rank -1
-        for tuple in counting:
-            vote += tuple[i]
-        return (vote) 
-
-test = Project (2)
-#test.counted_votes()
+compute_executable_projects (project_range)
 
 
-
-# class Most_Supported_Project:
-# # #Project with the most of votes
-#     def __init__ (self, project, vote):
-#         self.project = project
-#         self.vote = self.counted_votes()
-
-#     def get_best_project (self):
-    
+# output :
+# number of participants : 2
+# array of votes, per participants : [(10, 5), (1, 3)]
+# number of allocated budget : 2
+# [11]
