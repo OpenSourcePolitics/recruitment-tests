400ad37a9b40772503f3c969fd40409bda1f067d
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-11-06 12:53:09 +0100

    Delete Class Vote for function cep
    Test PASSED for 'No projects'

--- python/level-h/main.py
+++ python/level-h/main.py
@@ -3,29 +3,22 @@ import numpy as np
 # def compute_executable_projects(n, participants_votes, project_range):
 #     pass
 
-#input :
-details_set = (2, [(10,5),(0,3)],2) 
+# # #input :
+# details_set = (2, [(10,5),(0,3)], 2) 
 
-# expected output
-# == [1,2]
+# # # expected output
+# # # == [1,2]
 
-number_of_participants = details_set[0]
-participants_votes = details_set[1]
-number_of_supported_projects = details_set[2]
+# n = details_set[0]
+# participants_votes = details_set[1]
+# project_range = details_set[2]
 
 
-class Vote:
-
-    def __init__ (self):
-        self.number_of_participants = number_of_participants
-        self.participants_votes = participants_votes
-
-    def isChecked (self):
-        self.number_of_participants == len(self.participants_votes)
-        return 
-
-details_set = Vote
-print (bool(details_set.isChecked))
+def compute_executable_projects (n, participants_votes, project_range):
+    if project_range == 0:
+        return ('No projects, sorry!')
+    else:
+        return ('Votes : ok')
 
 
 class Project:
@@ -38,15 +31,13 @@ class Project:
         vote = 0
         i = self.rank -1
         for tuple in counting:
-                vote += tuple[i]
-        return (vote)
-        
+            vote += tuple[i]
+        return (vote) 
 
 test = Project (2)
-print(test.counted_votes())
+#test.counted_votes()
 
-# output :
-# 8
+
 
 # class Most_Supported_Project:
 # # #Project with the most of votes
@@ -55,21 +46,4 @@ print(test.counted_votes())
 #         self.vote = self.counted_votes()
 
 #     def get_best_project (self):
-
-# def compute_executable_projects (n, participants_votes, project_range):
-# # n = number of particpants
-    
-    
-#     set_votes = (tuple de votes)
-#     #tuples are ordered and unchangeable data
-
-#     project_range = number 
-
-
-
-#     if n != len(participants_votes):
-#         print ("It seems that we don't get all the votes !")
-
-#     if project_range == 0:
-#         print ("No projects, sorry")
     
