d6d852fd8d5dc5c937efbff3d6a257153901c589
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-11-12 16:04:39 +0100

    Code rewritten according to the requested tests !

--- ruby/level-o/main.rb
+++ ruby/level-o/main.rb
@@ -1,14 +1,15 @@
-# class Test
-#   def smallest_or_highest(arr, sort)
-#   end
-# end
+#class Test
 
-arr = [[34,15,88,2], 'anything else']
+ def smallest_or_highest(arr, sort)
+    if sort == 'min'
+      print arr.min
+    elsif sort == 'max'
+      print arr.max
+    else
+      print arr
+    end
+  end
 
-if arr[-1] == 'min'
-  print arr[0].min
-elsif arr[-1] == 'max'
-  print arr[0].max
-else
-  print arr[0]
-end
+smallest_or_highest([34,15,88,2], 'any')
+
+#end
