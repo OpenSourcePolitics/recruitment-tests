8233f5aa11e6382fc25202d5861aa9c29167e8eb
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-11-12 14:45:26 +0100

    Ok for the 3 conditions by visual tests

--- ruby/level-o/main.rb
+++ ruby/level-o/main.rb
@@ -3,8 +3,12 @@
 #   end
 # end
 
-arr = [[34,15,88,2], 'min']
+arr = [[34,15,88,2], 'anything else']
 
 if arr[-1] == 'min'
   print arr[0].min
+elsif arr[-1] == 'max'
+  print arr[0].max
+else
+  print arr[0]
 end
