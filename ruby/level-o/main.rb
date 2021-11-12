6665cc42220cb9cbab48446f4ab178d239dd6f4c
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-11-12 14:40:51 +0100

    Ok for 'min' by visual test

--- ruby/level-o/main.rb
+++ ruby/level-o/main.rb
@@ -1,4 +1,10 @@
-class Test
-  def smallest_or_highest(arr, sort)
-  end
+# class Test
+#   def smallest_or_highest(arr, sort)
+#   end
+# end
+
+arr = [[34,15,88,2], 'min']
+
+if arr[-1] == 'min'
+  print arr[0].min
 end
