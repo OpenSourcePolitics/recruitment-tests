5686b886b93e508aab184b761bc59010c0a9bc52
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-11-12 20:13:09 +0100

    Visual test ok for names in the test code
    But need to :
    - keep the numbers if there are
    - try to find easier way than full written hash

--- ruby/level-f/main.rb
+++ ruby/level-f/main.rb
@@ -1,4 +1,43 @@
-class Test
-  def rot13(string)
-  end
-end
+# class Test
+#   def rot13(string)
+
+string = 'Ada Lovelace'
+string.downcase!
+
+substitution = {
+  'a' => 'n',
+  'b' => 'o',
+  'c' => 'p',
+  'd' => 'q',
+  'e' => 'r',
+  'f' => 's',
+  'g' => 't',
+  'h' => 'u',
+  'i' => 'v',
+  'j' => 'w',
+  'k' => 'x',
+  'l' => 'y',
+  'm' => 'z',
+  'n' => 'a',
+  'o' => 'b',
+  'p' => 'c',
+  'q' => 'd',
+  'r' => 'e',
+  's' => 'f',
+  't' => 'g',
+  'u' => 'h',
+  'v' => 'i',
+  'w' => 'j',
+  'x' => 'k',
+  'y' => 'l',
+  'z' => 'm'
+}
+
+convert_string = string.gsub(/\w/,substitution)
+
+final_name = convert_string.gsub(/\S+/) {|word| word.capitalize}
+# keep in mind this other syntax :
+# final_name = convert_string.gsub(/\S+/, &:capitalize)
+
+print final_name
+
