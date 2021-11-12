4e3c8afef2ccda7563d09cb2f7f47793a4b27bd1
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-11-12 16:47:50 +0100

    Manage to find a Fibonacci Sequence in Ruby
    Pseudo code based on the rabbits statement using exponential function.
    Regarding to the tests that don't match, research on Fibonacci.

--- ruby/level-a/main.rb
+++ ruby/level-a/main.rb
@@ -1,4 +1,24 @@
-class Test
-  def fibo(n)
+#class Test
+
+def fibonacci(n)
+  a = 0
+  b = 1
+
+  # Compute Fibonacci number in the desired position.
+  n.times do
+      temp = a
+      a = b
+      # Add up previous two numbers in sequence.
+      b = temp + b
   end
+
+  return a
 end
+
+# Write first 15 Fibonacci numbers in sequence.
+15.times do |n|
+  result = fibonacci(n)
+  puts result
+end
+
+#end
