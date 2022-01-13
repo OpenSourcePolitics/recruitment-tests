4177d72d3c5f6a17cb41e5bb47948b99244b3dd3
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2022-01-13 16:30:22 +0100

    Final test : OK !!

--- python/level-h/main.py
+++ python/level-h/main.py
@@ -2,13 +2,12 @@ import numpy as np
 from typing import List, Tuple
 
 
-
 # # #input example for visual tests :
-details_set = (3, [(2,3,4),(7,10,2),(0,0,0)], 1)
+# details_set = (3, [(2,3,4),(7,10,2),(0,0,0)], 1)
 
-n = details_set[0]
-participants_votes = details_set[1]
-project_range = details_set[2]
+# n = details_set[0]
+# participants_votes = details_set[1]
+# project_range = details_set[2]
 
 
 def votes_per_project(n : int, participants_votes : List[Tuple[int, int]], project_range : int)-> List[int]:
@@ -23,16 +22,14 @@ def votes_per_project(n : int, participants_votes : List[Tuple[int, int]], proje
         for counter, element in enumerate(next_votes):
             votes[counter + 1] += element
 
-    print ("votes :", votes)
+    print("votes :", votes)
     rank_by_votes = dict(sorted(votes.items(), key=lambda x:x[1], reverse = True))
 
-    print (list(rank_by_votes.keys())[:project_range])
+    return(list(rank_by_votes.keys())[:project_range])
 
 
 def compute_executable_projects (n, participants_votes, project_range):
     if project_range == 0:
-        print ('No projects, sorry!')
+        return('No projects, sorry!')
     else:
-        votes_per_project (n, participants_votes, project_range)
-
-compute_executable_projects (n, participants_votes, project_range)
+        return(votes_per_project(n, participants_votes, project_range))
