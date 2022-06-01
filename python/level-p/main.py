f3a515c111fb7895a81c8facdb334be46844f4b2
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-06-01 13:39:27 +0200

    Do Python tests

--- python/level-p/main.py
+++ python/level-p/main.py
@@ -1,2 +1,14 @@
 def fizzbuzz(n):
-    pass
+    array = [];
+    for i in range(1, n+1):
+        string = "";
+        if i % 3 == 0:
+            string += "fizz"
+        if i % 5 == 0: 
+            string += "buzz"
+        if string == "":
+            string = str(i)
+        array.append(string)
+    return array
+pass
+
