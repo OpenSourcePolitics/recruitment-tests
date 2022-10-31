59ed14d8ca832ccfdea71f3fec34de61e75d45e0
THIVEND <baptiste.thivend@protonmail.com>
2022-10-31 07:53:16 +0100

    Modify level-t tests

--- python/level-t/test_main.py
+++ python/level-t/test_main.py
@@ -1,8 +1,14 @@
 import pytest
 from main import is_palindrome
 
-def test_is_palindrome():
+def test_single_word_palindrome():
     assert is_palindrome("kayak")
+
+def test_false_single_word_palindrome():
     assert not is_palindrome("success")
+
+def test_sentence_palindrome():
     assert is_palindrome("Sator Arepo Tenet Opera Rotas")
+
+def test_sentence_with_punctuation_palindrome():
     assert is_palindrome("Tu l'as trop ecrase Cesar, ce port-salut!")
